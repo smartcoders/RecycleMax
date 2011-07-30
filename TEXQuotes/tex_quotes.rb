fd453f9a6823ec9f5ca46c0116a28cbc6819c969
Alisson Sales <alisson.sales@gmail.com>
2011-07-30 19:00:56 -0300

    first commit - pain in the ass

---
+++ TEXQuotes/tex_quotes.rb
@@ -0,0 +1,32 @@
+require 'rubygems'
+require 'rspec'
+
+class Tex
+  attr_accessor :text
+  
+  def initialize(text)
+    @text = text
+  end
+  
+  def compile
+    matches = @text.match(/"(.+)"/)
+    if matches
+      @text = "``#{matches[1]}''"
+    end
+  end
+end
+
+describe Tex do
+  it 'should have a text' do
+    text = "my dam'n text"
+    tex = Tex.new text
+    tex.text.should == text
+  end
+    
+  it 'should translate the text to tex format' do
+    original_text = 'my phrase with "my quote"'
+    compiled_text = "my phrase with ``my quote''"
+    tex = Tex.new original_text
+    tex.compile.should == compiled_text 
+  end
+end
